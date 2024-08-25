2640bf2a760ea83f4bdc07c7b80c4491da1d99e6
Ahem <87135144+Ahemtan@users.noreply.github.com>
2024-08-25 21:30:08 +0545

    User Verification Completed

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
-import { CreateUserInput } from "../schema/user.schema";
-import { createUser } from "../services/user.service";
+import { CreateUserInput, VerifyUserInput } from "../schema/user.schema";
+import { createUser, findUserById } from "../services/user.service";
 import sendEmail from "../utils/mailer";
 
 export async function createUserHandler(
@@ -27,4 +27,30 @@ export async function createUserHandler(
 
     return res.status(500).send(error);
   }
+}
+
+export async function verifyUserHandler(req : Request<VerifyUserInput>, res: Response) {
+  const id = req.params.id
+  const verificationCode = req.params.verificationCode
+
+  const user = await findUserById(id)
+
+  if(!user) {
+    return res.send('Could not verify user.')
+  }
+
+  if(user.verified) {
+    return res.send("User is already verified")
+  }
+
+  if(user.verificationCode === verificationCode) {
+    user.verified = true
+
+    await user.save()
+
+    return res.send("User successfully verified");
+
+  }
+
+  return res.send("Could not verify user")  
 }
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -3,3 +3,9 @@ import UserModel, { User } from "../model/user.model";
 export function createUser(input: Partial<User>) {
     return UserModel.create(input);
 }
+
+export async function findUserById(id: string) {
+
+    return UserModel.findById(id)
+    
+}
