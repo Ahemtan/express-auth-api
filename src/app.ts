fd34d40ed43b32e91c39ca50d15024eefcd3987d
ahemtan <ahemtan1045@gmail.com>
2025-03-06 10:12:24 +0545

    Added Cors

--- src/app.ts
+++ src/app.ts
@@ -2,18 +2,27 @@ require("dotenv").config();
 
 import express from "express";
 import config from "config";
-var cookieParser = require('cookie-parser')
+import cookieParser from "cookie-parser";
 
 import logger from "./utils/logger";
 
 import router from "./routes";
 import deserializeUser from "./middleware/deserializeUser";
 
+import cors from "cors";
 
 const app = express();
 
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  cors({
+    origin: process.env.APP_ORIGIN,
+    credentials: true,
+  })
+);
 
 app.use(cookieParser());
 app.use(deserializeUser);
@@ -23,5 +32,5 @@ app.use(router);
 const port = config.get<number>("port");
 
 app.listen(port, async () => {
-  logger.info(`App is running on port ${port}`);
+  logger.info(`App is running on port ${process.env.ORIGIN_URL}`);
 });
